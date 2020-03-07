4008d099ac385c257d7d2d2659cb7caad016a83a
techtigers33958 <53841748+techtigers33958@users.noreply.github.com>
2020-03-07 10:05:40 -0500

    Commented code

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
+// Send message to start challenge, wait for the
+// smiley and then begin
 input.onButtonPressed(Button.B, function () {
     radio.sendString("START")
     basic.showIcon(IconNames.Happy)
 })
+// Send message to challenge and wait for the check
+// icon to tell if it is correct
 input.onButtonPressed(Button.A, function () {
     radio.sendString("hello world")
     basic.showIcon(IconNames.Yes)
